b3cf6993764cf33705b310395a947e3987e423cc
unknown <apratim dutta choudhury>
2020-08-18 19:22:26 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Cards from './Components/Cards/Cards';
+import Charts from './Components/Charts/Charts';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
+
+import { fetchData } from './api/Index';
+import styles from './App.module.css';
+
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
